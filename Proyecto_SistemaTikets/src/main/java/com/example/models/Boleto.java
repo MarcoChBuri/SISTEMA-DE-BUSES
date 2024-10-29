79b18fc96cf2d1ef914ca5e9e90ba4e92a50ce42
Marco Omar Chiliguano Buri <marco.chiliguano@unl.edu.ec>
2024-10-29 10:32:08 -0500

    BOLETO

--- Proyecto_SistemaTikets/src/main/java/com/example/models/Boleto.java
+++ Proyecto_SistemaTikets/src/main/java/com/example/models/Boleto.java
@@ -1,5 +1,50 @@
 package com.example.models;
 
 public class Boleto {
-    
+    private Integer id;
+    private Date fecha;
+    private float precioFinal;
+    private idBus idBus;
+    private Persona persona;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return this.fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public float getPrecioFinal() {
+        return this.precioFinal;
+    }
+
+    public void setPrecioFinal(float precioFinal) {
+        this.precioFinal = precioFinal;
+    }
+
+    public idBus getIdBus() {
+        return this.idBus;
+    }
+
+    public void setIdBus(idBus idBus) {
+        this.idBus = idBus;
+    }
+
+    public Persona getPersona() {
+        return this.persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
 }
