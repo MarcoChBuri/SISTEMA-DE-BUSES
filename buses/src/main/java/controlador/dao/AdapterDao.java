cc41f743f7d9b59db9b87337543e7b1d3d62a4cf
Dilan1615 <dilan.chambac@unl.edu.ec>
2025-02-02 11:41:53 -0500

    Cambios en el Adapter

--- buses/src/main/java/controlador/dao/AdapterDao.java
+++ buses/src/main/java/controlador/dao/AdapterDao.java
@@ -4,6 +4,7 @@ import controlador.tda.lista.LinkedList;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import java.lang.reflect.Method;
 import com.google.gson.Gson;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -50,12 +51,15 @@ public class AdapterDao<T> implements InterfazDao<T> {
 
     public void merge(T object, Integer index) throws Exception {
         LinkedList<T> list = listAll();
-        if (index < 0 || index >= list.getSize()) {
+        if (index >= 0 && index < list.getSize()) {
+            list.update(object, index);
+            Gson gson = new Gson();
+            String jsonListString = gson.toJson(list.toArray());
+            saveFile(jsonListString);
+        }
+        else {
             throw new Exception("Índice fuera de rango");
         }
-        list.update(object, index);
-        String info = g.toJson(list.toArray());
-        saveFile(info);
     }
 
     public void persist(T object) throws Exception {
@@ -98,6 +102,39 @@ public class AdapterDao<T> implements InterfazDao<T> {
         catch (Exception e) {
             System.out.println("Error al escribir en el archivo: " + e.getMessage());
             throw e;
+        }
+    }
+
+    protected Integer obtenerSiguienteId() throws Exception {
+        LinkedList<T> items = listAll();
+        if (items.isEmpty())
+            return 1;
+        try {
+            Method getIdMethod = null;
+            for (Method method : clazz.getMethods()) {
+                if (method.getName().startsWith("getId_")) {
+                    getIdMethod = method;
+                    break;
+                }
+            }
+            if (getIdMethod == null)
+                return items.getSize() + 1;
+            boolean[] usedIds = new boolean[items.getSize() + 2];
+            for (int i = 0; i < items.getSize(); i++) {
+                T item = items.get(i);
+                Integer id = (Integer) getIdMethod.invoke(item);
+                if (id != null && id <= usedIds.length) {
+                    usedIds[id - 1] = true;
+                }
+            }
+            for (int i = 0; i < usedIds.length; i++) {
+                if (!usedIds[i])
+                    return i + 1;
+            }
+            return items.getSize() + 1;
+        }
+        catch (Exception e) {
+            throw new Exception("Error generando ID: " + e.getMessage());
         }
     }
 }
