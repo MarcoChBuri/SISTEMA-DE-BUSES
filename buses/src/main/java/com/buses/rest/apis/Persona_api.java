7819911770170911db30f40f20493be5526b1343
Dilan1615 <166522911+Dilan1615@users.noreply.github.com>
2025-02-02 12:01:40 -0500

    Modificacion del Api Persona

--- buses/src/main/java/com/buses/rest/apis/Persona_api.java
+++ buses/src/main/java/com/buses/rest/apis/Persona_api.java
@@ -1,13 +1,16 @@
 package com.buses.rest.apis;
 
 import controlador.servicios.Controlador_persona;
+import controlador.servicios.Controlador_pago;
 import controlador.dao.modelo_dao.Pago_dao;
+import controlador.tda.lista.LinkedList;
+import controlador.dao.utiles.Fecha;
 import modelo.enums.Identificacion;
 import modelo.enums.Estado_cuenta;
 import javax.ws.rs.core.MediaType;
-import java.text.SimpleDateFormat;
 import javax.ws.rs.core.Response;
 import modelo.enums.Tipo_cuenta;
+import modelo.enums.Tipo_tarifa;
 import modelo.enums.Opcion_pago;
 import javax.ws.rs.PathParam;
 import java.util.Collections;
@@ -16,11 +19,11 @@ import javax.ws.rs.Consumes;
 import modelo.enums.Genero;
 import javax.ws.rs.DELETE;
 import java.util.HashMap;
+import java.util.Arrays;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.GET;
-import java.util.Date;
 import modelo.Persona;
 import modelo.Cuenta;
 import modelo.Pago;
@@ -68,36 +71,39 @@ public class Persona_api {
         HashMap<String, Object> response = new HashMap<>();
         Controlador_persona cp = new Controlador_persona();
         try {
-            String fechaOriginal = map.get("fecha_nacimiento").toString();
-            SimpleDateFormat formatoEntrada = new SimpleDateFormat("yyyy-MM-dd");
-            SimpleDateFormat formatoSalida = new SimpleDateFormat("dd/MM/yyyy");
-            Date fecha = formatoEntrada.parse(fechaOriginal);
-            String fechaFormateada = formatoSalida.format(fecha);
-            cp.getPersona().setNombre(map.get("nombres").toString());
-            cp.getPersona().setApellido(map.get("apellidos").toString());
-            cp.getPersona().setDireccion(map.get("direccion").toString());
+            String fechaNacimiento = map.get("fecha_nacimiento").toString();
+            String fechaNormalizada = Fecha.normalizarFecha(fechaNacimiento);
             cp.getPersona().setTipo_identificacion(
                     Identificacion.valueOf(map.get("tipo_identificacion").toString()));
-            cp.getPersona().setNumero_identificacion(map.get("num_identificacion").toString());
-            cp.getPersona().setFecha_nacimiento(fechaFormateada);
-            cp.getPersona().setTelefono(map.get("telefono").toString());
-            cp.getPersona().setCorreo(map.get("correo").toString());
+            cp.getPersona().setNumero_identificacion(map.get("numero_identificacion").toString());
+            cp.getPersona().setNombre(map.get("nombre").toString());
+            cp.getPersona().setApellido(map.get("apellido").toString());
             cp.getPersona().setGenero(Genero.valueOf(map.get("genero").toString()));
+            cp.getPersona().setCorreo(map.get("correo").toString());
+            cp.getPersona().setTelefono(map.get("telefono").toString());
+            cp.getPersona().setDireccion(map.get("direccion").toString());
+            cp.getPersona().setFecha_nacimiento(fechaNormalizada);
+            cp.getPersona().setTipo_tarifa(Tipo_tarifa.valueOf(map.get("tipo_tarifa").toString()));
             cp.getPersona().setSaldo_disponible(Float.parseFloat(map.get("saldo_disponible").toString()));
             Cuenta cuenta = new Cuenta();
             cuenta.setCorreo(map.get("usuario").toString());
-            cuenta.setContrasenia(map.get("clave").toString());
-            cuenta.setTipo_cuenta(Tipo_cuenta.Cliente);
-            cuenta.setEstado_cuenta(Estado_cuenta.Activo);
+            cuenta.setContrasenia(map.get("contrasenia").toString());
+            if (cp.Lista_personas().isEmpty()) {
+                cuenta.setTipo_cuenta(Tipo_cuenta.Administrador);
+            }
+            else {
+                cuenta.setTipo_cuenta(Tipo_cuenta.valueOf(map.get("tipo_cuenta").toString()));
+            }
+            cuenta.setEstado_cuenta(Estado_cuenta.valueOf(map.get("estado_cuenta").toString()));
             cp.getPersona().setCuenta(cuenta);
             if (map.containsKey("metodo_pago")) {
                 Pago pago = new Pago();
-                pago.setSaldo(Float.parseFloat(map.get("saldo").toString()));
+                pago.setOpcion_pago(Opcion_pago.valueOf(map.get("opcion_pago").toString()));
                 pago.setNumero_tarjeta(map.get("numero_tarjeta").toString());
                 pago.setTitular(map.get("titular").toString());
                 pago.setFecha_vencimiento(map.get("fecha_vencimiento").toString());
                 pago.setCodigo_seguridad(map.get("codigo_seguridad").toString());
-                pago.setOpcion_pago(Opcion_pago.valueOf(map.get("metodo_pago").toString()));
+                pago.setSaldo(Float.parseFloat(map.get("saldo").toString()));
                 cp.getPersona().setMetodo_pago(pago);
             }
             if (cp.save()) {
@@ -136,6 +142,42 @@ public class Persona_api {
         }
     }
 
+    @Path("/eliminarpago/{id}")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response eliminarMetodoPago(@PathParam("id") Integer id) {
+        HashMap<String, Object> response = new HashMap<>();
+        try {
+            Controlador_persona cp = new Controlador_persona();
+            Persona persona = cp.get(id);
+            if (persona == null) {
+                response.put("msg", "Persona no encontrada");
+                return Response.status(Response.Status.NOT_FOUND).entity(response).build();
+            }
+            Integer idPago = null;
+            if (persona.getMetodo_pago() != null) {
+                idPago = persona.getMetodo_pago().getId_pago();
+            }
+            persona.setMetodo_pago(null);
+            cp.setPersona(persona);
+            if (cp.update()) {
+                if (idPago != null) {
+                    Controlador_pago controladorPago = new Controlador_pago();
+                    controladorPago.delete(idPago);
+                }
+                response.put("msg", "Método de pago eliminado correctamente");
+                return Response.ok(response).build();
+            }
+            response.put("msg", "Error al eliminar el método de pago");
+            return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+        }
+        catch (Exception e) {
+            response.put("msg", "Error: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(response).build();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
     @Path("/actualizar")
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
@@ -153,21 +195,22 @@ public class Persona_api {
                 response.put("msg", "Error: Persona no encontrada");
                 return Response.status(Response.Status.NOT_FOUND).entity(response).build();
             }
-            persona.setNombre(getStringValue(map, "nombres", persona.getNombre()));
-            persona.setApellido(getStringValue(map, "apellidos", persona.getApellido()));
-            persona.setDireccion(getStringValue(map, "direccion", persona.getDireccion()));
+            String fechaNormalizada = Fecha.normalizarFecha(map.get("fecha_nacimiento").toString());
             persona.setTipo_identificacion(Identificacion.valueOf(
                     getStringValue(map, "tipo_identificacion", persona.getTipo_identificacion().toString())));
             persona.setNumero_identificacion(
-                    getStringValue(map, "num_identificacion", persona.getNumero_identificacion()));
-            persona.setFecha_nacimiento(
-                    getStringValue(map, "fecha_nacimiento", persona.getFecha_nacimiento()));
+                    getStringValue(map, "numero_identificacion", persona.getNumero_identificacion()));
+            persona.setNombre(getStringValue(map, "nombre", persona.getNombre()));
+            persona.setApellido(getStringValue(map, "apellido", persona.getApellido()));
+            persona.setDireccion(getStringValue(map, "direccion", persona.getDireccion()));
+            persona.setFecha_nacimiento(fechaNormalizada);
             persona.setTelefono(getStringValue(map, "telefono", persona.getTelefono()));
             persona.setCorreo(getStringValue(map, "correo", persona.getCorreo()));
             persona.setGenero(Genero.valueOf(getStringValue(map, "genero", persona.getGenero().toString())));
+            persona.setTipo_tarifa(Tipo_tarifa
+                    .valueOf(getStringValue(map, "tipo_tarifa", persona.getTipo_tarifa().toString())));
             persona.setSaldo_disponible(Float.parseFloat(getStringValue(map, "saldo_disponible", "0.0")));
             if (map.containsKey("cuenta")) {
-                @SuppressWarnings("unchecked")
                 HashMap<String, Object> cuentaMap = (HashMap<String, Object>) map.get("cuenta");
                 if (cuentaMap != null) {
                     Cuenta cuenta = persona.getCuenta();
@@ -191,28 +234,43 @@ public class Persona_api {
                 }
             }
             if (map.containsKey("metodo_pago")) {
-                @SuppressWarnings("unchecked")
                 HashMap<String, Object> pagoMap = (HashMap<String, Object>) map.get("metodo_pago");
                 if (pagoMap != null) {
                     Pago pago = persona.getMetodo_pago();
                     if (pago == null) {
                         pago = new Pago();
+                        pago.setOpcion_pago(Opcion_pago.valueOf(pagoMap.get("opcion_pago").toString()));
+                        pago.setTitular(pagoMap.get("titular").toString());
+                        pago.setNumero_tarjeta(pagoMap.get("numero_tarjeta").toString());
+                        pago.setFecha_vencimiento(pagoMap.get("fecha_vencimiento").toString());
+                        pago.setCodigo_seguridad(pagoMap.get("codigo_seguridad").toString());
+                        pago.setSaldo(Float.parseFloat(pagoMap.get("saldo").toString()));
+                        Pago_dao pagoDao = new Pago_dao();
+                        pagoDao.setPago(pago);
+                        if (pagoDao.save()) {
+                            persona.setMetodo_pago(pago);
+                        }
+                        else {
+                            throw new Exception("Error al guardar el nuevo método de pago");
+                        }
                     }
-                    Object pagoId = pagoMap.get("id_pago");
-                    if (pagoId != null && !pagoId.toString().equals("0")) {
-                        pago.setId_pago(Integer.valueOf(pagoId.toString()));
+                    else {
+                        Object pagoId = pagoMap.get("id_pago");
+                        if (pagoId != null && !pagoId.toString().equals("0")) {
+                            pago.setId_pago(Integer.valueOf(pagoId.toString()));
+                        }
+                        pago.setOpcion_pago(Opcion_pago.valueOf(
+                                getStringValue(pagoMap, "opcion_pago", pago.getOpcion_pago().toString())));
+                        pago.setNumero_tarjeta(getStringValue(pagoMap, "numero_tarjeta", ""));
+                        pago.setTitular(getStringValue(pagoMap, "titular", ""));
+                        pago.setFecha_vencimiento(getStringValue(pagoMap, "fecha_vencimiento", ""));
+                        pago.setCodigo_seguridad(getStringValue(pagoMap, "codigo_seguridad", ""));
+                        pago.setSaldo(Float.parseFloat(getStringValue(pagoMap, "saldo", "0.0")));
+                        Pago_dao pagoDao = new Pago_dao();
+                        pagoDao.setPago(pago);
+                        pagoDao.update();
+                        persona.setMetodo_pago(pago);
                     }
-                    pago.setSaldo(Float.parseFloat(getStringValue(pagoMap, "saldo", "0.0")));
-                    pago.setNumero_tarjeta(getStringValue(pagoMap, "numero_tarjeta", ""));
-                    pago.setTitular(getStringValue(pagoMap, "titular", ""));
-                    pago.setFecha_vencimiento(getStringValue(pagoMap, "fecha_vencimiento", ""));
-                    pago.setCodigo_seguridad(getStringValue(pagoMap, "codigo_seguridad", ""));
-                    pago.setOpcion_pago(
-                            Opcion_pago.valueOf(getStringValue(pagoMap, "opcion_pago", "Tarjeta_credito")));
-                    Pago_dao pagoDao = new Pago_dao();
-                    pagoDao.setPago(pago);
-                    pagoDao.update();
-                    persona.setMetodo_pago(pago);
                 }
             }
             cp.setPersona(persona);
@@ -244,30 +302,136 @@ public class Persona_api {
     @Produces(MediaType.APPLICATION_JSON)
     public Response actualizarMetodoPago(HashMap<String, Object> map) {
         HashMap<String, Object> response = new HashMap<>();
-        Controlador_persona cp = new Controlador_persona();
         try {
+            Controlador_persona cp = new Controlador_persona();
+            Controlador_pago controladorPago = new Controlador_pago();
             cp.setPersona(cp.get(Integer.parseInt(map.get("id_persona").toString())));
             Pago pago = cp.getPersona().getMetodo_pago();
             if (pago == null) {
                 pago = new Pago();
             }
-            pago.setSaldo(Float.parseFloat(map.get("saldo").toString()));
+            pago.setOpcion_pago(Opcion_pago.valueOf(map.get("opcion_pago").toString()));
+            Integer idPago = pago.getId_pago();
             pago.setNumero_tarjeta(map.get("numero_tarjeta").toString());
             pago.setTitular(map.get("titular").toString());
             pago.setFecha_vencimiento(map.get("fecha_vencimiento").toString());
             pago.setCodigo_seguridad(map.get("codigo_seguridad").toString());
-            pago.setOpcion_pago(Opcion_pago.valueOf(map.get("metodo_pago").toString()));
-            cp.getPersona().setMetodo_pago(pago);
-            if (cp.update()) {
-                response.put("msg", "Método de pago actualizado correctamente");
-                response.put("persona", cp.getPersona());
-                return Response.ok(response).build();
+            if (map.get("saldo") != null) {
+                if (map.get("saldo") instanceof Number) {
+                    pago.setSaldo(((Number) map.get("saldo")).floatValue());
+                }
+                else {
+                    pago.setSaldo(Float.parseFloat(map.get("saldo").toString()));
+                }
+            }
+            if (idPago != null) {
+                pago.setId_pago(idPago);
+            }
+            controladorPago.setPago(pago);
+            if (controladorPago.update()) {
+                cp.getPersona().setMetodo_pago(controladorPago.getPago());
+                if (cp.update()) {
+                    response.put("msg", "Método de pago actualizado correctamente");
+                    response.put("persona", cp.getPersona());
+                    return Response.ok(response).build();
+                }
             }
             response.put("msg", "Error al actualizar el método de pago");
             return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
         }
         catch (Exception e) {
+            e.printStackTrace();
             response.put("msg", "Error: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(response).build();
+        }
+    }
+
+    @Path("/transferir-saldo")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response transferirSaldo(HashMap<String, Object> map) {
+        HashMap<String, Object> response = new HashMap<>();
+        try {
+            if (!map.containsKey("id_persona")) {
+                response.put("msg", "ID de persona requerido");
+                return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+            }
+            Integer idPersona = Integer.parseInt(map.get("id_persona").toString());
+            Controlador_persona cp = new Controlador_persona();
+            Persona persona = cp.get(idPersona);
+            if (persona == null) {
+                response.put("msg", "Persona no encontrada");
+                return Response.status(Response.Status.NOT_FOUND).entity(response).build();
+            }
+            if (persona.getMetodo_pago() == null) {
+                response.put("msg", "La persona no tiene método de pago");
+                return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+            }
+            Float saldoTarjeta = persona.getMetodo_pago().getSaldo();
+            Float saldoDisponible = persona.getSaldo_disponible();
+            persona.setSaldo_disponible(saldoDisponible + saldoTarjeta);
+            persona.getMetodo_pago().setSaldo(0.0f);
+            cp.setPersona(persona);
+            if (cp.update()) {
+                response.put("msg", "Saldo transferido correctamente");
+                response.put("persona", persona);
+                return Response.ok(response).build();
+            }
+            response.put("msg", "Error al transferir el saldo");
+            return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+        }
+        catch (Exception e) {
+            response.put("msg", "Error: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(response).build();
+        }
+    }
+
+    @Path("/ordenar/{atributo}/{orden}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response ordenarPersonas(@PathParam("atributo") String atributo,
+            @PathParam("orden") String orden) {
+        HashMap<String, Object> response = new HashMap<>();
+        try {
+            Controlador_persona cp = new Controlador_persona();
+            LinkedList<Persona> lista = cp.Lista_personas();
+            if (!Arrays
+                    .asList("tipo_identificacion", "numero_identificacion", "nombre", "apellido",
+                            "fecha_nacimiento", "genero", "telefono", "correo", "metodo_pago.opcion_pago",
+                            "cuenta.correo", "cuenta.tipo_cuenta", "cuenta.estado_cuenta")
+                    .contains(atributo)) {
+                response.put("mensaje", "Atributo de ordenamiento no válido");
+                return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+            }
+            lista.quickSort(atributo, orden.equalsIgnoreCase("asc"));
+            response.put("mensaje", "Lista ordenada correctamente");
+            response.put("personas", lista.toArray());
+            return Response.ok(response).build();
+        }
+        catch (Exception e) {
+            response.put("mensaje", "Error al ordenar personas: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(response).build();
+        }
+    }
+
+    @Path("/buscar/{atributo}/{criterio}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response buscarPersona(@PathParam("atributo") String atributo,
+            @PathParam("criterio") String criterio) {
+        HashMap<String, Object> response = new HashMap<>();
+        try {
+            Controlador_persona cp = new Controlador_persona();
+            LinkedList<Persona> lista = cp.Lista_personas();
+            LinkedList<Persona> resultados = lista.binarySearch(criterio, atributo);
+            response.put("mensaje", "Búsqueda realizada");
+            response.put("cuentas", resultados.toArray());
+            return Response.ok(response).build();
+        }
+        catch (Exception e) {
+            response.put("mensaje", "Error en la búsqueda");
+            response.put("error", e.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(response).build();
         }
     }
